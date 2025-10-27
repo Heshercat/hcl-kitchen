dbd762712bc359b530872fda9c8c4dc385368441
Alfiia Barylenko <98765399+Heshercat@users.noreply.github.com>
2025-10-28 01:15:44 +0200

    Update S3 bucket name tag for dry run check

--- aws-resources/aws-simple-plans/simple_s3.tf
+++ aws-resources/aws-simple-plans/simple_s3.tf
@@ -19,7 +19,7 @@ resource "aws_s3_bucket" "example" {
   force_destroy = true
 
   tags = {
-    Name        = "My bucket"
+    Name        = "My bucket - Check Dry run"
     Environment = "Test"
   }
 }
